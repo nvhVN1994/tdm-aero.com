21992eb5c0df301ae7ae9c126be5947c08b14b24
NVHVN94 <hoangnv1994.work@gmail.com>
2025-07-24 11:58:36 +0700

    1234

--- src/Pages/GlobalOfficesPage.js
+++ src/Pages/GlobalOfficesPage.js
@@ -16,15 +16,15 @@ export default function GlobalOfficesPage() {
    
         <div className="bg-white text-center px-4 py-10">
           <h3 className="text-lg font-semibold mt-2 xl:text-[15px] xl:font-bold xl:mt-4 px-4 py-1">
-            VIETNAM (HEADQUARTERS): +84 38 636 073/ sales@tdmgroup.vn                                  
+            VIETNAM (HEADQUARTERS): Nguyen Huu Truong +84 38 636 073/ sales@tdmgroup.vn                                  
         <div className="bg-white text-center px-4 py-1"></div>
-            NEW ZEALAND: +64 274 798 096/ brett@tdmgroup.vn
+            NEW ZEALAND: Brett Waghorn +64 274 798 096/ brett@tdmgroup.vn
         <div className="bg-white text-center px-4 py-1"></div>
-            AUSTRLIA: +61 423 488 044/ chloe@tdmgroup.vn 
+            AUSTRLIA: Chloe Nguyen +61 423 488 044/ chloe@tdmgroup.vn 
         <div className="bg-white text-center px-4 py-1"></div>
-            MALAYSIA: +60 13 777 6870/ buvan@tdmgroup.vn
+            MALAYSIA: Buvanesan Tamil Selvam +60 13 777 6870/ buvan@tdmgroup.vn
         <div className="bg-white text-center px-4 py-1"></div>
-            GERMANY: 
+            GERMANY: Hoai Thu Nguyen +49 40 752 56554/ thu@tdmgroup.vn
           </h3>
           <div className="mt -1 flex justify-center">
             <img
