ccd830b0bb123a09bdbfaa7eae9276290b529f9f
NVHVN94 <hoangnv1994.work@gmail.com>
2025-07-24 11:14:42 +0700

    1234

--- src/Pages/GlobalOfficesPage.js
+++ src/Pages/GlobalOfficesPage.js
@@ -13,14 +13,20 @@ export default function GlobalOfficesPage() {
         <h2 className="text-center text-2xl xl:text-4xl font-medium mb-8 mt-8">
         </h2>
 
-
-
-        {/* Section 5: Our Global Presence */}
+   
         <div className="bg-white text-center px-4 py-10">
-          <h3 className="text-lg font-semibold mt-2 xl:text-[30px] xl:font-bold xl:mt-4 px-4 py-1">
-            OUR GLOBAL PRESENCE
+          <h3 className="text-lg font-semibold mt-2 xl:text-[15px] xl:font-bold xl:mt-4 px-4 py-1">
+            VIETNAM (HEADQUARTERS): + 84 38 636 073                                   
+        <div className="bg-white text-center px-4 py-1"></div>
+            NEW ZEALAND: + 64 210 275 7289
+        <div className="bg-white text-center px-4 py-1"></div>
+            AUSTRLIA: 
+        <div className="bg-white text-center px-4 py-1"></div>
+            MALAYSIA: 
+        <div className="bg-white text-center px-4 py-1"></div>
+            GERMANY: 
           </h3>
-          <div className="mt-8 flex justify-center">
+          <div className="mt -1 flex justify-center">
             <img
               src={MapChart}
             alt="Map Chart"
